4743539a2d87927574ad040e0e2b2491542173cd
Deepak Sharma <sharma.deepak8616@gmail.com>
2021-12-15 21:52:45 -0500

    class-1 homework

---
+++ src/test/java/Class1/Homework1.java
@@ -0,0 +1,22 @@
+package Class1;
+
+public class Homework1 {
+
+    /**
+     * Create new repository in github
+     * Invite Ahsan, Tim or I as collaborator in the repository
+     * Clone repository in local
+     * Create a Maven project in above clones repository
+     * Add TestNg and Selenium-Java dependencies in pom.xml
+     * Check version of Chrome installed in your machine
+     * Download and save compatible ChromeDriver
+     * Create a new package in src/test/java package
+     * Create a new class in above created package
+     * Create a test-method using @Test annotation
+     * Write code in above test-method to perform below steps
+     * 1. Launch amazon.com
+     * 2. verify url of the launched is as expected
+     *
+     */
+
+}
